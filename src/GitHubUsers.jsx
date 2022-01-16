408daa44e50cb86db05ac6956e14f7871fd8a239
Himanshu Kumar Singh <himanshu.kr5294@gmail.com>
2022-01-16 16:19:45 +0530

    user info added in state

--- src/GitHubUsers.jsx
+++ src/GitHubUsers.jsx
@@ -5,16 +5,27 @@ export const GithubUsers = () => {
   const [userName, setUserName] = useState("");
   const [userRepoList, setUserRepoList] = useState([]);
 
+  const [showFollowers, setShowFollowers] = useState(false);
+  const [showRepoList, setShowRepoList] = useState(false);
+  const [owner, setOwner] = useState([]);
+  const [totalRepos, setTotalRepos] = useState([]);
+
   const getUserName = (e) => {
     setUserName(e.target.value);
   };
 
-  const getUserRepositories = () => {
+  const getUserRepositories = (e) => {
+    setShowFollowers(false);
+    const user = e.target.id;
+
     axios
-      .get(`https://api.github.com/users/${userName}/repos`)
+      .get(`https://api.github.com/users/${user}/repos`)
       .then((res) => {
         console.log(`res`, res.data);
         if (res.data.length !== 0) {
+          setShowRepoList(true);
+          setOwner(res.data[0].owner);
+          setTotalRepos(res.data.length);
           setUserRepoList(res.data);
         } else {
           console.log("User has no repos...");
@@ -30,7 +41,9 @@ export const GithubUsers = () => {
       <div className="App">
         <h2>Github User Repositories</h2>
         <input type="text" onChange={getUserName} />
-        <button onClick={getUserRepositories}>Search</button>
+        <button onClick={getUserRepositories} id={userName}>
+          Search
+        </button>
       </div>
     </div>
   );
